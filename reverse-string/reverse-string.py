b8d34d15488c021f84f2274a06d9544144aeb68d
Abhishek Premnath <abhishekprem0212@gmail.com>
2021-12-01 11:06:43 -0800

    Update reverse-string.py

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -4,10 +4,9 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         j=len(s)-1
-        for i in range(j+1):
+        for i in range((j+1)//2):
             s[i],s[j]=s[j],s[i]
             j-=1
-            if j<=i:
-                break
+           
         
         
