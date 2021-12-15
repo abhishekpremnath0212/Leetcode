7d04a2888b94fd01f81eb57ed5987fe87aea790c
Abhishek Premnath <abhishekprem0212@gmail.com>
2021-12-14 17:25:02 -0800

    Time: 234 ms (77.01%), Space: 21 MB (93.31%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -4,12 +4,9 @@ class Solution:
         ans=[None]*l
         ans[0]=1
         for i in range(1,l):
-            ans[i]=ans[i-1]*nums[i-1]#all numbers to the left of the number
-        
-        p=1#using a variable instead of another list
+            ans[i]=nums[i-1]*ans[i-1]
+        tmp=1
         for i in range(l-1,-1,-1):
-            ans[i]=ans[i]*p#all numbers to the right of the number
-            p=p*nums[i]
-        
+            ans[i]=ans[i]*tmp
+            tmp=tmp*nums[i]
         return ans
-        
